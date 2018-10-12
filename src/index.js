032ad02faff48d0c76051b29099d6778b63d50f3
Rohan Kapur <me@rohankapur.com>
2018-10-12 06:15:12 -0700

    la

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render((
-	<HttpsRedirect>
+	<Router>
 		<App />
-	</HttpsRedirect>
+	</Router>
 ), document.getElementById('root'));
 
 //registerServiceWorker();
